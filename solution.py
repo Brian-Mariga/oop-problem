977a05603d8ee386102f2a664a9d58a64d7a96b3
Brian <brianmariga0@gmail.com>
2024-03-19 08:24:10 +0300

    Completed assignment

---
+++ solution.py
@@ -0,0 +1,11 @@
+def scoreboard(participants):
+    results =[]
+    for participant in participants:
+        score = participant['chickenwings']*5 + participant['hamburgers']*3 + participant['hotdogs']*2
+        results.append({'name': participant['name'], 'score': score})
+    print(results)
+
+scoreboard([
+    {'name': "Habanero Hillary", 'chickenwings': 5, 'hamburgers': 17, 'hotdogs': 11},
+    {'name': "Big Bob", 'chickenwings': 20, 'hamburgers': 4, 'hotdogs': 11}
+])
