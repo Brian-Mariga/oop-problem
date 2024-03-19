e9806430f15f574e6d30f4ff23ceff3dbc20567b
Brian <brianmariga0@gmail.com>
2024-03-19 08:49:07 +0300

    Completed assignment

--- solution.py
+++ solution.py
@@ -1,7 +1,10 @@
 def scoreboard(participants):
     results =[]
     for participant in participants:
-        score = participant['chickenwings']*5 + participant['hamburgers']*3 + participant['hotdogs']*2
+        score = 0
+        score += participant.get('chickenwings', 0) * 5
+        score += participant.get('hamburgers', 0) * 3
+        score += participant.get('hotdogs', 0) * 2
         results.append({'name': participant['name'], 'score': score})
     results.sort(key=lambda x: (-x['score'], x['name']))
     print(results)
@@ -9,5 +12,6 @@ def scoreboard(participants):
 
 scoreboard([
     {'name': "Habanero Hillary", 'chickenwings': 5, 'hamburgers': 17, 'hotdogs': 11},
-    {'name': "Big Bob", 'chickenwings': 20, 'hamburgers': 4, 'hotdogs': 11}
+    {'name': "Big Bob", 'chickenwings': 20, 'hamburgers': 4, 'hotdogs': 11},
+    {'name': "Small Bob", 'chickenwings': 20, 'hamburgers': 4, 'hotdogs': 11}
 ])
